f77660df3b6929261f5517bd5f21a4857fb5f921
sohamsawant <sohamsawant.2010@gmail.com>
2025-12-09 13:25:22 -0800

    history features 5th

--- app/src/androidTest/java/com/example/calorietracker/HistoryFeaturesTest.java
+++ app/src/androidTest/java/com/example/calorietracker/HistoryFeaturesTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-
 public class HistoryFeaturesTest {
     private String calculateStatus(int totalCalories, int limit) {
         if (totalCalories < limit) {
