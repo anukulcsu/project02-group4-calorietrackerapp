7fc2cbe9af1ba5a48fbe7b186616030d98d5801b
Anukul Shet <ashet@csumb.edu>
2025-12-09 21:28:28 -0800

    Added Intent Tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.firestore)
+    implementation(libs.core)
+    implementation(libs.ext.junit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
