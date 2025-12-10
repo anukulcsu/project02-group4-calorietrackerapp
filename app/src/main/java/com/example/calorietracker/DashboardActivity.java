5a69684912f6126fa5f18a8d69796d35ea0fd98b
sohamsawant <sohamsawant.2010@gmail.com>
2025-12-09 18:01:50 -0800

    tips final 6th

--- app/src/main/java/com/example/calorietracker/DashboardActivity.java
+++ app/src/main/java/com/example/calorietracker/DashboardActivity.java
@@ -76,7 +76,6 @@ public class DashboardActivity extends AppCompatActivity {
         calorieCountView = findViewById(R.id.calorieCount);
         targetDisplayView = findViewById(R.id.targetDisplay);
         comparisonView = findViewById(R.id.comparison);
-
         int caloriesConsumed = Integer.parseInt(calorieCountView.getText().toString());
         updateComparison(caloriesConsumed);
 
